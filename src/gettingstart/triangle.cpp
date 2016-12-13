e8c37ea21eb20f1755519c1a669ec262f8d3d3d9
luojianghua <luojianghua@pset.suntec.net>
2016-12-13 17:34:56 +0800

    put things to lib bin dir

--- src/gettingstart/triangle.cpp
+++ src/gettingstart/triangle.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <iostream>
 
+#include "utils.h"
+
 GLfloat vertices[] = {
     -0.5f, 0.5f, 0.0f,
     -0.5f, -0.5f, 0.0f,
@@ -59,77 +61,6 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
     if (GLFW_KEY_ESCAPE == key && GLFW_PRESS == action) {
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
-}
-
-GLenum glCheckError_(const char *file, int line)
-{
-    GLenum errorCode;
-    while ((errorCode = glGetError()) != GL_NO_ERROR)
-    {
-        printf("---------!!!error occurs!!!-------------------\n");
-        printf("error code: %d\n", errorCode);
-        std::string error;
-        switch (errorCode)
-        {
-            case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
-            case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
-            case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
-            case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
-            case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
-            case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
-        }
-        std::cout << error << " | " << file << " (" << line << ")" << std::endl;
-    }
-    return errorCode;
-}
-#define glCheckError() glCheckError_(__FILE__, __LINE__)
-
-void APIENTRY glDebugOutput(GLenum source,
-                            GLenum type,
-                            GLuint id,
-                            GLenum severity,
-                            GLsizei length,
-                            const GLchar *message,
-                            const GLvoid *userParam)
-{
-    if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
-
-    printf("------------------\n");
-    printf("Debug Message:%d %s\n", id, message);
-
-    switch (source)
-    {
-        case GL_DEBUG_SOURCE_API:             printf("Source: API"); break;
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   printf("Source: Window System"); break;
-        case GL_DEBUG_SOURCE_SHADER_COMPILER: printf("Source: Shader Compiler"); break;
-        case GL_DEBUG_SOURCE_THIRD_PARTY:     printf("Source: Third Party"); break;
-        case GL_DEBUG_SOURCE_APPLICATION:     printf("Source: Application"); break;
-        case GL_DEBUG_SOURCE_OTHER:           printf("Source: Other"); break;
-    }
-
-    switch (type)
-    {
-        case GL_DEBUG_TYPE_ERROR:               printf("Type: Error"); break;
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: printf("Type: Deprecated Behaviour"); break;
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  printf("Type: Undefined Behaviour"); break; 
-        case GL_DEBUG_TYPE_PORTABILITY:         printf("Type: Portability"); break;
-        case GL_DEBUG_TYPE_PERFORMANCE:         printf("Type: Performance"); break;
-        case GL_DEBUG_TYPE_MARKER:              printf("Type: Marker"); break;
-        case GL_DEBUG_TYPE_PUSH_GROUP:          printf("Type: Push Group"); break;
-        case GL_DEBUG_TYPE_POP_GROUP:           printf("Type: Pop Group"); break;
-        case GL_DEBUG_TYPE_OTHER:               printf("Type: Other"); break;
-    }
-
-    switch (severity)
-    {
-        case GL_DEBUG_SEVERITY_HIGH:         printf("Severity: high"); break;
-        case GL_DEBUG_SEVERITY_MEDIUM:       printf("Severity: medium"); break;
-        case GL_DEBUG_SEVERITY_LOW:          printf("Severity: low"); break;
-        case GL_DEBUG_SEVERITY_NOTIFICATION: printf("Severity: notification"); break;
-    }
-
-    printf("\n\n");
 }
 
 GLuint createShaderProgram()
